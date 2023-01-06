1da02611bdd76a0d50c9ad464a47e9e595fd97a5
Abdullahi254 <abdush504@gmail.com>
2023-01-06 15:55:59 +0300

    secured api keys in a .env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,13 +10,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDfXRMXUigL_83-sTRThSP1FnkecD9lla4",
-  authDomain: "project-watiti.firebaseapp.com",
-  projectId: "project-watiti",
-  storageBucket: "project-watiti.appspot.com",
-  messagingSenderId: "487704313096",
-  appId: "1:487704313096:web:523376a9b0e3dbf24500bd",
-  measurementId: "G-T262RJWR5Z"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_AUTH_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_AUTH_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
